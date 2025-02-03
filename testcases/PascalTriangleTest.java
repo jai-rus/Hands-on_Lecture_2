1a9ac8544d284f8f8262b82b4d056d01421e7c3f
Jairus <jairus.legion@gmail.com>
2025-02-02 16:38:08 -0800

    Updated Pascal Triangle Test to include more tests for both brute force and memo solution
    Included more test cases for brute force and memo

--- testcases/PascalTriangleTest.java
+++ testcases/PascalTriangleTest.java
@@ -1,22 +1,36 @@
-import java.util.List;
-
 public class PascalTriangleTest {
         public static void main(String[] args) {
             // Test 1: 5 rows using brute force
-            System.out.println("Test Case 1 - Brute Force (5 rows)");
+            System.out.println("Test Case 1 - 5 Rows Brute Force");
             PascalsTriangle.printTriangle(PascalsTriangle.generateBruteForce(5));
 
             // Test 2: 5 rows using memoization
-            System.out.println("\nTest Case 2 - Memoization (5 rows)");
+            System.out.println("\nTest Case 2 - 5 Rows Memoization");
             PascalsTriangle.printTriangle(PascalsTriangle.generateMemo(5));
 
             // Test 3: Edge Case (1 row)
-            System.out.println("\nTest Case 3 - 1 Row");
+            System.out.println("\nTest Case 3 - 1 Row Brute Force");
             PascalsTriangle.printTriangle(PascalsTriangle.generateBruteForce(1));
 
             // Test 4: Edge Case (0 rows)
-            System.out.println("\nTest Case 4 - 0 Rows");
+            System.out.println("\nTest Case 4 - 0 Rows Brute Force");
             PascalsTriangle.printTriangle(PascalsTriangle.generateBruteForce(0));
+
+            // Test 5: Edge Case (1 row)
+            System.out.println("\nTest Case 3 - 1 Row Memoization");
+            PascalsTriangle.printTriangle(PascalsTriangle.generateMemo(1));
+
+            // Test 6: Edge Case (0 rows)
+            System.out.println("\nTest Case 4 - 0 Rows Memoization");
+            PascalsTriangle.printTriangle(PascalsTriangle.generateMemo(0));
+
+            // Test 7: 20 rows using brute force
+            System.out.println("\nTest Case 1 - 5 Rows Brute Force");
+            PascalsTriangle.printTriangle(PascalsTriangle.generateBruteForce(20));
+
+            // Test 8: 20 rows using memoization
+            System.out.println("\nTest Case 2 - 5 Rows Memoization");
+            PascalsTriangle.printTriangle(PascalsTriangle.generateMemo(20));
         }
     }
 
